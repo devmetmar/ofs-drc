426b0d4961de9cd8ca6929d13aaf42e02e5c986f
yothunder <tyo.suwignyo@gmail.com>
2025-11-07 07:04:34 +0000

    refine workflow

--- inawaves/run-prod.sh
+++ inawaves/run-prod.sh
@@ -5,7 +5,7 @@
 # - Avoid running SLURM command in relation with accessing/linking to /data
 # 
 # Created in October 2025
-# Contact: suwignyo.prasetyo@bmkg.go.id
+# Contact: tyo.suwignyo@gmail.com
 
 set -e
 source /etc/profile.d/modules.sh
@@ -14,6 +14,7 @@ module load mpi/2021.5.1
 module load compiler/2022.0.2
 source ${HOME}/ofs-prod/inawaves/env.sh
 start_time=$(date +%s)
+time_start=$(date --date "@${start_time}" +"%Y-%m-%d %H:%M:%S")
 send_telegram() {
     local message="$1"
     curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
@@ -88,15 +89,16 @@ check_job_status() {
 logging "########################################################################"
 logging "                       WRAPPER FOR INAWAVES MODEL                      "
 logging "------------------------------------------------------------------------"
+logging "MODELCYCLE     : ${NWDAY} - ${CYCLE}"
 logging "ROOTDIR        : ${RDIR}"
 logging "WORKDIR        : ${WDIR}"
 logging "LIBRARIES      : ${LIBRARIES}"
-logging "PYTHON         : ${PYTHON}"
 logging "PYTHONPATH     : $(which python)"
 logging "INDATA         : ${INDATA}"
 logging "OUTDATA        : ${OUDATA}"
 logging "LOGFILE        : ${log_file}"
-logging "MODELCYCLE     : ${NWDAY} - ${CYCLE}"
+logging "PATH           : ${PATH}"
+logging "LD_LIBRARY_PATH: ${LD_LIBRARY_PATH}"
 logging "########################################################################"
 
 
@@ -104,14 +106,16 @@ logging "-----------------------------------------------------------------------
 logging "                             PREPROCESSING                              "
 logging "------------------------------------------------------------------------"
 
-# ${PYTHON} -u ${WDIR}/prep/grib2ww3.py -t 384 ${INDATA}/gfs ${INDATA}/gfs/w3g_gfs.txt archive ${NWDAY}${CYCLE} >> $log_file 2>&1
+${PYTHON} -u ${WDIR}/prep/grib2ww3.py -t 384 ${INDATA}/gfs ${INDATA}/gfs/w3g_gfs.txt archive ${NWDAY}${CYCLE} >> $log_file 2>&1
 
 rm -rf ${WDIR}/prep/wind.txt
 rm -rf ${WDIR}/prep/mod_def*
-cp -f ${INDATA}/gfs/w3g_gfs.txt ${WDIR}/prep/wind.txt --verbose >> $log_file 2>&1 &
-cp -f ${INDATA}/static/gfs/mod_def.ww3 ${WDIR}/prep/mod_def.ww3 --verbose >> $log_file 2>&1 &
+cp -f ${INDATA}/gfs/w3g_gfs.txt ${WDIR}/prep/wind.txt --verbose >> $log_file 2>&1
+cp -f ${INDATA}/static/gfs/mod_def.ww3 ${WDIR}/prep/mod_def.ww3 --verbose >> $log_file 2>&1
 cd ${WDIR}/prep
-# srun --ntasks=1 --exclude=drc0 --exclusive ${WDIR}/prep/ww3_prep >> $log_file 2>&1
+logging "PATH           : ${PATH}"
+logging "LD_LIBRARY_PATH: ${LD_LIBRARY_PATH}"
+srun --ntasks=1 --exclude=drc0 --exclusive ${WDIR}/prep/ww3_prep >> $log_file 2>&1
 cd ${WDIR}/main
 rm -rf ${WDIR}/main/wind.gfs
 ln -sf ${WDIR}/prep/wind.ww3 ${WDIR}/main/wind.gfs
@@ -149,57 +153,59 @@ rm -rf ${WDIR}/main/restart*
 
 logging "Copying mod_def files ..."
 rm -rf ${WDIR}/main/mod_def*
-cp -f ${INDATA}/static/gfs/mod_def.ww3 ${WDIR}/main/mod_def.gfs --verbose >> $log_file 2>&1 &
-cp -f ${INDATA}/static/global/mod_def.ww3 ${WDIR}/main/mod_def.global --verbose >> $log_file 2>&1 &
-cp -f ${INDATA}/static/reg/mod_def.ww3 ${WDIR}/main/mod_def.reg --verbose >> $log_file 2>&1 &
-cp -f ${INDATA}/static/sreg/mod_def.ww3 ${WDIR}/main/mod_def.sreg --verbose >> $log_file 2>&1 &
-cp -f ${INDATA}/static/hires/mod_def.ww3 ${WDIR}/main/mod_def.hires --verbose >> $log_file 2>&1 &
-cp -f ${INDATA}/static/points/mod_def.ww3 ${WDIR}/main/mod_def.points --verbose >> $log_file 2>&1 &
+cp -f ${INDATA}/static/gfs/mod_def.ww3 ${WDIR}/main/mod_def.gfs --verbose >> $log_file 2>&1
+cp -f ${INDATA}/static/global/mod_def.ww3 ${WDIR}/main/mod_def.global --verbose >> $log_file 2>&1
+cp -f ${INDATA}/static/reg/mod_def.ww3 ${WDIR}/main/mod_def.reg --verbose >> $log_file 2>&1
+cp -f ${INDATA}/static/sreg/mod_def.ww3 ${WDIR}/main/mod_def.sreg --verbose >> $log_file 2>&1
+cp -f ${INDATA}/static/hires/mod_def.ww3 ${WDIR}/main/mod_def.hires --verbose >> $log_file 2>&1
+cp -f ${INDATA}/static/points/mod_def.ww3 ${WDIR}/main/mod_def.points --verbose >> $log_file 2>&1
 
 logging "Copying new recent restart files ..."
 rm -rf ${WDIR}/main/restart*
-cp -f ${INDATA}/static/global/restart.ww3  ${WDIR}/main/restart.global --verbose >> $log_file 2>&1 &
-cp -f ${INDATA}/static/reg/restart.ww3     ${WDIR}/main/restart.reg --verbose    >> $log_file 2>&1 &
-cp -f ${INDATA}/static/sreg/restart.ww3    ${WDIR}/main/restart.sreg --verbose   >> $log_file 2>&1 &
-cp -f ${INDATA}/static/hires/restart.ww3   ${WDIR}/main/restart.hires --verbose  >> $log_file 2>&1 &
+cp -f ${INDATA}/static/global/restart.ww3  ${WDIR}/main/restart.global --verbose >> $log_file 2>&1 || logging "No global restart found, run cold start" 
+cp -f ${INDATA}/static/reg/restart.ww3     ${WDIR}/main/restart.reg --verbose    >> $log_file 2>&1 || logging "No reg restart found, run cold start"    
+cp -f ${INDATA}/static/sreg/restart.ww3    ${WDIR}/main/restart.sreg --verbose   >> $log_file 2>&1 || logging "No sreg restart found, run cold start"   
+cp -f ${INDATA}/static/hires/restart.ww3   ${WDIR}/main/restart.hires --verbose  >> $log_file 2>&1 || logging "No hires restart found, run cold start"  
 
-# logging "Submitting job run_multi.sh ..."
-# job_id=$(sbatch ${WDIR}/main/run_ww3multi.sh ${NWDAY}${CYCLE} | awk '{print $4}')
-# logging "Job ID: $job_id"
+logging "PATH           : ${PATH}"
+logging "LD_LIBRARY_PATH: ${LD_LIBRARY_PATH}"
+logging "Submitting job run_multi.sh ..."
+job_id=$(sbatch ${WDIR}/main/run_ww3multi.sh ${NWDAY}${CYCLE} | awk '{print $4}')
+logging "Job ID: $job_id"
 
-# sleep 10
+sleep 10
 
-# # Check if the job ID was captured successfully
-# if [ -z "$job_id" ]; then
-#     logging "Failed to submit the job."
-#     exit 1
-# fi
+# Check if the job ID was captured successfully
+if [ -z "$job_id" ]; then
+    logging "Failed to submit the job."
+    exit 1
+fi
 
-# while true; do
-#     job_status=$(check_job_status $job_id)
-#     logging "Job status: ${job_status}"  # Debug output
-#     if [ "${job_status}" == "" ]; then
-#         logging "ww3_multi job $job_id has completed."
-#         break
-#     fi
-#     logging "ww3_multi job $job_id is still running. Checking again in 60 seconds..."
-#     sleep 60
-# done
+while true; do
+    job_status=$(check_job_status $job_id)
+    logging "Job status: ${job_status}"  # Debug output
+    if [ "${job_status}" == "" ]; then
+        logging "ww3_multi job ${job_id} has completed."
+        break
+    fi
+    logging "ww3_multi job ${job_id} is still running. Checking again in 60 seconds..."
+    sleep 60
+done
 
-# logging "Model integration has finished."
-# cat log.mww3 >> $log_file
-# mv slurm* ${log_dir}
+logging "Model integration has finished."
+cat log.mww3 >> $log_file
+mv slurm* ${log_dir} >> $log_file 2>&1
 
 logging "Moving restart files to each domain directory ..."
-mv ${WDIR}/main/restart001.global ${INDATA}/static/global/restart.ww3 >> $log_file 2>&1 || logging "No global restart found, run cold start"
-mv ${WDIR}/main/restart001.reg    ${INDATA}/static/reg/restart.ww3 >> $log_file 2>&1 || logging "No reg restart found, run cold start"
-mv ${WDIR}/main/restart001.sreg   ${INDATA}/static/sreg/restart.ww3 >> $log_file 2>&1 || logging "No sreg restart found, run cold start"
-mv ${WDIR}/main/restart001.hires  ${INDATA}/static/hires/restart.ww3 >> $log_file 2>&1 || logging "No hires restart found, run cold start" 
+mv ${WDIR}/main/restart001.global ${INDATA}/static/global/restart.ww3 >> $log_file 2>&1 
+mv ${WDIR}/main/restart001.reg    ${INDATA}/static/reg/restart.ww3 >> $log_file 2>&1    
+mv ${WDIR}/main/restart001.sreg   ${INDATA}/static/sreg/restart.ww3 >> $log_file 2>&1   
+mv ${WDIR}/main/restart001.hires  ${INDATA}/static/hires/restart.ww3 >> $log_file 2>&1  
 
 logging "------------------------------------------------------------------------"
 logging "                             POST PROCESSING                            "
 logging "------------------------------------------------------------------------"
-NCOUT=$OUDATA/nc/inawaves/$year/$month/$year$month$day
+NCOUT=$OUDATA/nc/inawaves/$year/$month
 TMPIMOUT=${WDIR}/post/$year$month$day$CYCLE
 IMOUT=$OUDATA/img/inawaves/$year/$month
 logging "NETCDF OUT    : ${NCOUT}"
@@ -215,6 +221,7 @@ cp -f ${INDATA}/static/reg/mod_def.ww3 ${WDIR}/post/mod_def.reg --verbose >> $lo
 cp -f ${INDATA}/static/sreg/mod_def.ww3 ${WDIR}/post/mod_def.sreg --verbose >> $log_file 2>&1
 cp -f ${INDATA}/static/hires/mod_def.ww3 ${WDIR}/post/mod_def.hires --verbose >> $log_file 2>&1
 ln -sf ${WDIR}/main/out_grd* ${WDIR}/post >> $log_file 2>&1
+
 logging "                       CREATE GRADS OUTPUT SECTION                      "
 logging "------------------------------------------------------------------------"
 sed "
@@ -226,38 +233,47 @@ else
     logging "Failed editing gx_outf.inp"
     exit 1
 fi
+
+logging "PATH           : ${PATH}"
+logging "LD_LIBRARY_PATH: ${LD_LIBRARY_PATH}"
 logging "Run create_grads2.sh"
-# srun --ntasks=1 --exclude=drc0 --exclusive ${WDIR}/post/create_grads.sh >> $log_file 2>&1
+srun --ntasks=1 --exclude=drc0 --exclusive ${WDIR}/post/create_grads.sh >> $log_file 2>&1
 
 logging "                        CONVERT GRADS TO NETCDF                         "
 logging "------------------------------------------------------------------------"
 cd ${WDIR}/post
-# srun --ntasks=1 ${PYTHON} ${WDIR}/post/grads2nc.py global --modelcycle ${NWDAY}${CYCLE} >> $log_file 2>&1 &
-# srun --ntasks=1 ${PYTHON} ${WDIR}/post/grads2nc.py reg --modelcycle ${NWDAY}${CYCLE} >> $log_file 2>&1 &
-# srun --ntasks=1 ${PYTHON} ${WDIR}/post/grads2nc.py hires --modelcycle ${NWDAY}${CYCLE} >> $log_file 2>&1 &
+logging "PATH           : ${PATH}"
+logging "LD_LIBRARY_PATH: ${LD_LIBRARY_PATH}"
+srun --ntasks=1 ${PYTHON} ${WDIR}/post/grads2nc.py global --modelcycle ${NWDAY}${CYCLE} >> $log_file 2>&1 &
+srun --ntasks=1 ${PYTHON} ${WDIR}/post/grads2nc.py reg --modelcycle ${NWDAY}${CYCLE} >> $log_file 2>&1 &
+srun --ntasks=1 ${PYTHON} ${WDIR}/post/grads2nc.py hires --modelcycle ${NWDAY}${CYCLE} >> $log_file 2>&1 &
 wait
 
 logging "                               WW3 Plotting                             "
 logging "------------------------------------------------------------------------"
 ${PYTHON} ${WDIR}/post/plotter.py inawaves ${NWDAY}${CYCLE}  --out_dir $TMPIMOUT >> $log_file 2>&1
-#srun --ntasks=1 --cpus-per-task=48 --exclude=drc0 --exclusive 
 logging "Successfully running plotter"
 
 logging "Moving NC files to $NCOUT ..."
 mv ${WDIR}/post/*nc $NCOUT >> $log_file 2>&1
 logging "Moving IMG files to $IMOUT ..."
 mv ${TMPIMOUT} $IMOUT >> $log_file 2>&1
-rm ${WDIR}/post/*nc
 
 end_time=$(date +%s)
+time_end=$(date --date "@${end_time}" +"%Y-%m-%d %H:%M:%S")
 elapsed_time=$((end_time - start_time))
-logging "########################################################################"
-logging "                    SUCCESSFULLY RUNNING WW3 GFS                        "
-logging "Total elapsed time: ${elapsed_time} seconds                               "
-logging "Exiting                                                                 "
-logging "########################################################################"
 
 successmsg="WW3 DRC SUCCESS! Cycle: ${NWDAY}${CYCLE} Log: $log_file Elapsed: ${elapsed_time} seconds"
 send_telegram "${successmsg}"
+logging "Sent message to telegram: ${successmsg}"
+
+logging "########################################################################"
+logging "          SUCCESSFULLY RUNNING WAVEWATCH3 INAWAVES on DRC               "
+logging "------------------------------------------------------------------------"
+logging "Total elapsed time: ${elapsed_time} seconds                             "
+logging "Start time       : ${time_start}                                        "
+logging "End time         : ${time_end}                                          "
+logging "Exiting                                                                 "
+logging "########################################################################"
 
 exit 0
