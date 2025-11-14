e53a76317b91f5f11e08d61bd83c394d96c297f4
yothunder <tyo.suwignyo@gmail.com>
2025-11-14 13:50:33 +0000

    rewrite env

--- inawaves/env.sh
+++ inawaves/env.sh
@@ -1,17 +1,17 @@
-***REMOVED***
+#!/bin/bash
 
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
+RDIR="/home/model-admin/ofs-prod"
+WDIR="${RDIR}/inawaves"
+WW3DIR="$WDIR/WW3"
+LIBRARIES="${RDIR}/libraries"
+PYTHON="/home/model-admin/opt/miniforge3/envs/ofs/bin/python"
+INDATA="/data/ofs/input"
+OUDATA="/data/ofs/output"
+SCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SRCTMP="/tmp/inawaves-prod"
 
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
+export PATH=${LIBRARIES}/bin:$PATH
+export LD_LIBRARY_PATH=${LIBRARIES}/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/lib64:$LD_LIBRARY_PATH
+export NetCDF_ROOT=${LIBRARIES}/bin
+export NetCDF_INCLUDE_DIRS=${LIBRARIES}/include
